9f1e3959bca19cec897a0a231f6c8736dab169d8
DaCurse <dacurse0@gmail.com>
2021-12-04 22:23:36 +0200

    Added error handlers

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 import createDebug from 'debug';
 import express from 'express';
+import errorHandler from './middleware/error-handler.js';
+import notFoundHandler from './middleware/not-found-handler.js';
 import loadRoutes from './routes/index.js';
 
 const debug = createDebug('note-app:express');
@@ -9,6 +11,9 @@ async function createApp() {
 
   debug('Loading routes');
   await loadRoutes(app);
+
+  app.use(errorHandler);
+  app.use(notFoundHandler);
 
   return app;
 }
