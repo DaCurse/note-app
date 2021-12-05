1118f144c7a0859fb3a84fb25f2254a157460b12
DaCurse <dacurse0@gmail.com>
2021-12-05 23:48:36 +0200

    Added an endpoint to create notes

--- routes/notes.js
+++ routes/notes.js
@@ -1,7 +1,11 @@
 import { Router } from 'express';
 import validate from '../middleware/validate.js';
-import { getNoteByIdSchema, getNotesSchema } from '../schemas/notes.js';
-import { getNoteById, getNotes } from '../services/notes.js';
+import {
+  getNoteByIdSchema,
+  getNotesSchema,
+  noteDTOSchema,
+} from '../schemas/notes.js';
+import { createNote, getNoteById, getNotes } from '../services/notes.js';
 
 const router = Router();
 
@@ -11,6 +15,10 @@ router.get('/', validate(getNotesSchema, 'query'), async (req, res) =>
 
 router.get('/:id', validate(getNoteByIdSchema, 'params'), async (req, res) =>
   res.json(await getNoteById(req.params.id))
+);
+
+router.post('/', validate(noteDTOSchema, 'body'), async (req, res) =>
+  res.json(await createNote(req.body))
 );
 
 export const prefix = '/notes';
--- services/notes.js
+++ services/notes.js
@@ -11,3 +11,7 @@ export function getNoteById(id) {
     },
   });
 }
+
+export function createNote(noteDTO) {
+  return prisma.notes.create({ data: noteDTO });
+}
