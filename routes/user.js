2d8bf669ba605779d3cf072065c48077dc3eb75d
DaCurse <dacurse0@gmail.com>
2021-12-07 22:52:29 +0200

    Move conflict error handling to service

--- routes/user.js
+++ routes/user.js
@@ -1,27 +1,13 @@
-import Prisma from '@prisma/client';
-import { Router } from 'express';
-import createHttpError from 'http-errors';
 import { userDTO } from '../dto/user.js';
 import validate from '../middleware/validate.js';
 import { createUser } from '../services/user.js';
+import AsyncRouter from '../util/async-router.js';
 
-const router = Router();
+const router = new AsyncRouter();
 
-router.post('/register', validate(userDTO, 'body'), async (req, res, next) => {
-  try {
-    res.json(await createUser(req.body));
-  } catch (error) {
-    // TODO: Refactor error handling
-    if (error instanceof Prisma.PrismaClientKnownRequestError) {
-      switch (error.code) {
-        case 'P2002':
-          next(createHttpError(409, 'Username already exists'));
-      }
-    } else {
-      next(createHttpError(500, error));
-    }
-  }
-});
+router.post('/register', validate(userDTO, 'body'), async (req, res) =>
+  res.json(await createUser(req.body))
+);
 
 export const prefix = '/user';
 export { router };
