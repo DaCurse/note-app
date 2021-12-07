ba0305ba4f331f0e002931e1febbeb9aeb948698
DaCurse <dacurse0@gmail.com>
2021-12-07 22:52:29 +0200

    Move conflict error handling to service

--- services/user.js
+++ services/user.js
@@ -1,11 +1,22 @@
 import argon2 from 'argon2';
+import HttpErrors from 'http-errors';
 import prisma from '../providers/prisma.js';
+
+const { Conflict, InternalServerError } = HttpErrors;
 
 export async function createUser(userDTO) {
   const { username, password } = userDTO;
   const passwordHash = await argon2.hash(password);
-  return prisma.user.create({
-    select: { userId: true },
-    data: { username, passwordHash },
-  });
+  try {
+    return await prisma.user.create({
+      select: { userId: true },
+      data: { username, passwordHash },
+    });
+  } catch (error) {
+    console.log('123');
+    if (error?.code === 'P2002') {
+      throw new Conflict('Username already exists');
+    }
+    throw InternalServerError();
+  }
 }
