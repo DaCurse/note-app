3a2e90a5484176f45f23f3b0a8145f9a195c6f72
DaCurse <dacurse0@gmail.com>
2021-12-07 23:00:13 +0200

    Added login endpoint

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,16 @@
 import { userDTO } from '../dto/user.js';
 import validate from '../middleware/validate.js';
-import { createUser } from '../services/user.js';
+import { createUser, loginUser } from '../services/user.js';
 import AsyncRouter from '../util/async-router.js';
 
 const router = new AsyncRouter();
 
 router.post('/register', validate(userDTO, 'body'), async (req, res) =>
   res.json(await createUser(req.body))
+);
+
+router.post('/login', validate(userDTO, 'body'), async (req, res) =>
+  res.json(await loginUser(req.body))
 );
 
 export const prefix = '/user';
