5cdc5f0af50afed646da9ee8c37845bc20d7de34
DaCurse <dacurse0@gmail.com>
2021-12-18 23:18:00 +0200

    Added user authorization to all note routes

--- services/notes.js
+++ services/notes.js
@@ -1,22 +1,38 @@
 import prisma from '../providers/prisma.js'
 
-export function getNotes(limit) {
-  return prisma.note.findMany({ take: limit })
+export function getNotes(userId, limit) {
+  return prisma.note.findMany({ where: { userId }, take: limit })
 }
 
-export function getNoteById(id) {
-  return prisma.note.findUnique({
+export function getNoteById(userId, id) {
+  return prisma.note.findFirst({
     where: {
+      userId,
       noteId: id,
     },
   })
 }
 
-export function createNote(noteDTO) {
-  return prisma.note.create({ data: noteDTO })
+export function createNote(userId, noteDTO) {
+  return prisma.note.create({
+    data: {
+      user: {
+        connect: { userId },
+      },
+      ...noteDTO,
+    },
+  })
 }
 
-export function updateNote(id, noteDTO) {
+export async function updateNote(userId, id, noteDTO) {
+  // TODO: See if there is a way to do this in one query
+  await prisma.note.findFirst({
+    where: {
+      noteId: id,
+      userId,
+    },
+  })
+
   return prisma.note.update({
     where: {
       noteId: id,
